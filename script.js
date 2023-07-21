59b8c3607ab7b82a4cc06760632378cc639906eb
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-21 16:26:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
 
+	 let form = document.getElementById("form1");
+
+     let firstName = form.elements["fname"].value;
+
+	 let lastName = form.elements["lname"].value;
+
+	 alert(`${firstName} ${lastName}`)
+	
 }
